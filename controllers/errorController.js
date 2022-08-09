753bf5f8659fabd4e6b6fd7bea3eb0bc6eab6d31
lntrTin0212 <100136208+lntrTin0212@users.noreply.github.com>
2022-08-09 11:57:15 +0700

    update handling Error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,28 @@
+const AppError = require('../utils/appError');
+
+const handleCaseErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = err => {
+  const value = err.errmsg.match(/(["'])(?:(?=(\\?))\2.)*?\1/);
+  const message = `Duplicate field value: ${value} Please use another value!`;
+
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = err => {
+  // Object.values ex
+  // {'name': 'trong tin', 'age': 22}
+  // After use Object.values => ['trong tin', 22]
+
+  const errors = Object.values(err.errors).map(val => val.message);
+
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -23,12 +48,21 @@ const sendErrorProd = (err, res) => {
   }
 };
 
+// globalErrorHandler in app.js
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+
+    // Handling invalid ID
+    if (error.name === 'CastError') error = handleCaseErrorDB(error);
+    // Handling duplicated filed unique
+    if (error.code === 111000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
     sendErrorProd(err, res);
   }
 };
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', err => {
+  console.log('Uncaught Exception, Shutting Down... ');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -20,6 +26,16 @@ mongoose
   });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLER REJECTION, Shutting Down... ');
+  // use server.close to give time to finish all the request
+  // after killed the server
+  server.close(() => {
+    process.exit(1);
+  });
 });
