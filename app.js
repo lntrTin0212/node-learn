2c93161a712041cad3bf3872d3b44b72ee2cbf24
lntrTin0212 <100136208+lntrTin0212@users.noreply.github.com>
2022-08-15 15:01:49 +0700

    prevent

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoutes');
@@ -37,6 +38,19 @@ app.use(express.json());
 app.use(mongoSanitize());
 // Data sanitization against XSS
 app.use(xss());
+// Prevent Parameter Pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+);
 
 // serving static file
 app.use(express.static(`${__dirname}/public`));
