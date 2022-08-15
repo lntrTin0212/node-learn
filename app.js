30ade4bcd7a5e2e481f169a918aab886a552b63d
lntrTin0212 <100136208+lntrTin0212@users.noreply.github.com>
2022-08-15 14:11:20 +0700

    Data Sanitization

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoutes');
@@ -30,6 +32,12 @@ app.use('/api', limiter);
 
 // body parser, reading data from body into req.body
 app.use(express.json());
+
+// Data sanitization against NoSQL query injecttion
+app.use(mongoSanitize());
+// Data sanitization against XSS
+app.use(xss());
+
 // serving static file
 app.use(express.static(`${__dirname}/public`));
 
