e03353bbc55c8fa9f9152009806c5e0c13e7133e
lntrTin0212 <100136208+lntrTin0212@users.noreply.github.com>
2022-08-16 21:26:36 +0700

    Nested Routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,10 +2,14 @@ const express = require('express');
 
 const tourController = require('./../controllers/tourController');
 const authController = require('./../controllers/authController');
+const reviewRouter = require('./../routes/reviewRoutes');
 
 const router = express.Router();
 
-// router.param('id', tourController.checkID);
+// POST /tour/:tourId/reviews
+// GET /tour/:tourId/reviews
+// GET /tour/:tourId/reviews/:reviewsId
+router.use('/:tourId/reviews', reviewRouter);
 
 router
   .route('/top-5-cheap')
