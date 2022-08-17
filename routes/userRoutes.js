9ee317508e61671e7ac2b245e4dd1d3a8c5e14b1
lntrTin0212 <100136208+lntrTin0212@users.noreply.github.com>
2022-08-17 13:30:41 +0700

    Adding missing authentication

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,27 +5,21 @@ const authController = require('./../controllers/authController');
 const router = express.Router();
 
 router.post('/signup', authController.signup);
-
 router.post('/login', authController.login);
-
 router.post('/forgotPassword', authController.forgotPassword);
-
 router.patch('/resetPassword/:token', authController.resetPassword);
 
-router.patch(
-  '/updatePassowrd',
-  authController.protect,
-  authController.updatePassword
-);
+// protect all the routes that come after this point.
+router.use(authController.protect);
 
-router.get(
-  '/me',
-  authController.protect,
-  userController.getMe,
-  userController.getUser
-);
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
+router.patch('/updatePassowrd', authController.updatePassword);
+
+router.get('/me', userController.getMe, userController.getUser);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
+
+// and restricted all the routes only to the admin after this point
+router.use(authController.restrictTo('admin'));
 
 router
   .route('/')
